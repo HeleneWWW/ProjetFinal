30999c253b20ef4b7a54a792edc597175b484dbe
GaelleLC <57903519+GaelleLC@users.noreply.github.com>
2019-12-17 13:02:38 +0100

    search(); update container

--- public/views/pages/search.php
+++ public/views/pages/search.php
@@ -1,27 +1,27 @@
 
 <?php ob_start(); ?>
 
-<h1>Votre recherche</h1>
-<?php var_dump($nom); ?>
+<div class="container">
 
-<?php if($tag): ?>
-<h1>Résultat de votre recherche pour le tag : <?=$tag[0]['t_nom']; ?></h1>
-    <?php foreach($tag as $info): ?>
-<a href ="<?= url('site/'.$site['s_id'].'/'.$site['s_slug']); ?>"><h6><?= $info['s_nom']; ?></h6></a>
-    <?php endforeach; ?>
-<?php else: ?>
+    <h1>Votre recherche</h1>
+        <hr/>
+    <?php var_dump($nom); ?>
 
-    <?php if($nom): ?>
+    <?php if($tag): ?>
+    <h1>Résultat de votre recherche pour le tag : <?=$tag[0]['t_nom']; ?></h1>
         <?php foreach($tag as $info): ?>
-
+    <a href ="<?= url('site/'.$site['s_id'].'/'.$site['s_slug']); ?>"><h6><?= $info['s_nom']; ?></h6></a>
         <?php endforeach; ?>
+    <?php else: ?>
+
+        <?php if($nom): ?>
+            <?php foreach($tag as $info): ?>
+
+            <?php endforeach; ?>
+        <?php endif; ?>
+
     <?php endif; ?>
 
-<?php endif; ?>
-
-
-
-
-
+</div>
 
 <?php $content = ob_get_clean() ?> <?php view('template', compact('content')); ?>
