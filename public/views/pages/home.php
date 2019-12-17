9f995dbbbdb941e3df9534530b1f405ac99b8653
Uranie31 <57894146+Uranie31@users.noreply.github.com>
2019-12-17 17:20:22 +0100

    Mise à jour Home Masonry

--- public/views/pages/home.php
+++ public/views/pages/home.php
@@ -10,7 +10,7 @@
 <!-- Test Affichage Isotope Library -->
 <div class="grid">
   <div class="grid-item"></div>
-  <div class="grid-item grid-item--width2 grid-item--height2"><img class="w-100" src="<?= img_url('HTML_logo.svg') ?>"  style="height: 100%;">0</div>
+  <div class="grid-item grid-item--width2 grid-item--height2">0</div>
   <div class="grid-item grid-item--height3">1</div>
   <div class="grid-item grid-item--height2">2</div>
   <div class="grid-item grid-item--width3">3</div>
@@ -19,7 +19,7 @@
   <div class="grid-item grid-item--height2">6</div>
   <div class="grid-item grid-item--width2 grid-item--height3"><img class="w-100" src="<?= img_url('PHP_logo.svg') ?>"  style="height: 100%;">7</div>
   <div class="grid-item">20</div>
-  <div class="grid-item grid-item--height2">8</div>
+  <div class="grid-item grid-item--height2"><img class="w-100" src="<?= img_url('AJAX_logo.svg') ?>"  style="height: 100%;"></div>
   <div class="grid-item"><img class="w-100" src="<?= img_url('dh.png') ?>">21</div>
   <div class="grid-item grid-item--width2 grid-item--height2">9</div>
   <div class="grid-item grid-item--width2">10</div>
@@ -28,7 +28,7 @@
   <div class="grid-item">13</div>
   <div class="grid-item">14</div>
   <div class="grid-item grid-item--height3">15</div>
-  <div class="grid-item grid-item--height2">16</div>
+  <div class="grid-item grid-item--height2"><img class="w-100" src="<?= img_url('CSS_logo.png') ?>"  style="height: 85%;"></div>
   <div class="grid-item">17</div>
   <div class="grid-item">18</div>
   <div class="grid-item grid-item--height2">19</div>
