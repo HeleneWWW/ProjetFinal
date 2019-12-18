129ddceae10865daff9195dd7b4bb5f20d312fc5
Uranie31 <57894146+Uranie31@users.noreply.github.com>
2019-12-18 15:53:34 +0100

    tag home quasi fini

--- public/views/pages/home.php
+++ public/views/pages/home.php
@@ -7,11 +7,11 @@
 </div>
 <!-- Logos Tags -->
 
-<?php foreach ($tags as $tag):?>
+<div class="d-flex justify-content-center "><?php foreach ($tags as $tag):?>
     <a  href="<?= url('tagsite/'.$tag['t_id'].'/'.$tag['t_slug']); ?>" class="">
-        <img src="<?= img_url($tag['t_slug'].$tag['t_img']); ?>" width="20%" alt="">
+        <img src="<?= img_url($tag['t_slug'].$tag['t_img']); ?>" width="70%" alt="">
     </a>
-<?php endforeach; ?>
+<?php endforeach; ?></div>
     
 
 <?php $content = ob_get_clean() ?> <?php view('template', compact('content')); ?>
