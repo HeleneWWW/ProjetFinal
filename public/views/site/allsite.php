de1400f49e3978658293a3d83bfc83a6156c3b1b
HeleneWWW <57895572+HeleneWWW@users.noreply.github.com>
2019-12-18 10:13:07 +0100

    bdd lastupdate

--- public/views/site/allsite.php
+++ public/views/site/allsite.php
@@ -15,38 +15,46 @@
         <hr/>
         <?=  (!empty($alert)) ? $alert : '';  ?>
 
-    <!-- <div class="mt-4">
-        <?php foreach ($sites as $site): ?>
-            <div class="row mb-2">
-                <div class="col-2">
-                    <img src="<?= img_url($site['s_slug'].$site['s_imgindex']); ?>" width="100%" alt="">
-                </div>
-                <div class="col-10">
-                    <a href="<?= url('site/'.$site['s_id'].'/'.$site['s_slug']); ?>"><?= $site['s_nom']; ?></a><br>
-                    
-                </div>
-            </div>
-        <?php endforeach; ?>
-     -->
 
+        
 
-    <?php foreach ($sites as $site): ?>
-
-<div class="card" style="width: 18rem;">
+<!-- <?php foreach ($sites as $site): ?>
+<div class="card bd-highlight col-md-2 no-wrap">
   <img src="<?= img_url($site['s_slug'].$site['s_imgindex']); ?>" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title"><?= $site['s_nom']; ?></h5>
     <a href="<?= url('site/'.$site['s_id'].'/'.$site['s_slug']); ?>" class="btn btn-primary">Plus d'info</a>
     </div>
+     </div>
+<?php endforeach; ?> -->
+
+
+
+<div class="container">
+
+<div class="card-group">
+  <div class="card">
+
+    <img src="<?= img_url($site['s_slug'].$site['s_imgindex']); ?>" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title"><?= $site['s_nom']; ?></h5>
+     
+      
+      <a href="<?= url('site/'.$site['s_id'].'/'.$site['s_slug']); ?>" class="btn btn-primary">Plus d'info</a>
+
     </div>
   </div>
-   
-   
-    <?php endforeach; ?>
-   
+  
+</div>
+
+</div>
 
 
 
+
+
+
+   
 
 <?php $content = ob_get_clean() ?> 
 <?php view('template', compact('content')); ?>
