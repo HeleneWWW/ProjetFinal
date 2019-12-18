3e8ec29f15fe675a3b24b060d1add7954a0700f7
GaelleLC <57903519+GaelleLC@users.noreply.github.com>
2019-12-18 11:21:42 +0100

    update one site

--- public/views/site/onesite.php
+++ public/views/site/onesite.php
@@ -7,25 +7,28 @@
 ?>
 
 <?php ob_start(); ?>
-<div class='container'>
-<div class='row' id= "public-onesite">
-   <a href="<?= $site['s_url']; ?>">
-            <img id='onesite-icon' src="<?= img_url($site['s_slug'].$site['s_imgindex']); ?>" width="50%" alt="">
-   <h6><?= $site['s_nom'];  ?></h6>
-</a>
-    <p><?= $site['s_description']; ?></p>
-    <?php foreach ($images as $img):?>
-    <img src="<?= img_url($site['s_slug'].$img['sm_url']); ?>" width="100%" alt="">
-    <?php endforeach; ?>
-    <h6>Tags associés : </h6>
-    <?php foreach ($tags as $key => $tag):?>
-    <td><?= $tag['t_nom'];?></td>
-    <?php endforeach; ?>
-    <img src="" width="100%" alt="">
-    
-   
-</div>
-</div>
+    <div class='container'>
+        <div class='' id= "public-onesite">
+            <a href="<?= $site['s_url']; ?>" class="p-5" target="_blank">
+                <img id='onesite-icon' src="<?= img_url($site['s_slug'].$site['s_imgindex']); ?>" class="" alt="">
+                <h3 class=""><?= $site['s_nom'];  ?></h3>
+            </a>
+            
+            <p class="" ><?= $site['s_description']; ?></p>
+
+            <?php foreach ($images as $img):?>
+                <img src="<?= img_url($site['s_slug'].$img['sm_url']); ?>" width="100%" alt="">
+            <?php endforeach; ?>
+
+            <hr/><h6>Tags associés : </h6>
+
+            <?php foreach ($tags as $key => $tag):?>
+                <td><?= $tag['t_nom'];?></td>
+            <?php endforeach; ?>
+
+            <img src="" width="100%" alt="">
+        </div>
+    </div>
 
 <?php $content = ob_get_clean() ?> 
 <?php view('template', compact('content')); ?>
