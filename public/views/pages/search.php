e204e404582d097a72cee42f0fa29876a2e29231
HeleneWWW <57895572+HeleneWWW@users.noreply.github.com>
2019-12-19 10:33:58 +0100

    css details

--- public/views/pages/search.php
+++ public/views/pages/search.php
@@ -5,27 +5,34 @@
 
     <h1>Votre recherche</h1>
         <hr/>
-
+    
     <?php if($tag): ?>
 
     <h1>Sites associés au tag : <?=$tag[0]['t_nom']; ?></h1>
+
         <?php foreach($tag as $info): ?>
-        <img src="<?= img_url($info['s_slug'].$info['s_imgindex']); ?>" width="10%" alt="">
+            <div class='searchbox d-block '>
+        <img src="<?= img_url($info['s_slug'].$info['s_imgindex']); ?>" width="50%" alt="">
     <a href ="<?= url('site/'.$info['s_id'].'/'.$info['s_slug']); ?>"><h6><?= $info['s_nom']; ?></h6></a>
+            </div>    
         <?php endforeach; ?>
-        
+       
     <?php else: ?>
 
         <?php if($nom):  ?>
-        <h1>Résultat de votre recherche pour '<?= $_GET['site'];?>' </h1>            
+        <h1>Résultat de votre recherche pour '<?= $_GET['site'];?>' </h1>      
+      
+      
             <?php foreach($nom as $info): ?>
-            <img src="<?= img_url($info['s_slug'].$info['s_imgindex']); ?>" width="10%" alt="">
-    <a href ="<?= url('site/'.$info['s_id'].'/'.$info['s_slug']); ?>"><h6><?= $info['s_nom']; ?></h6></a>
+                <div class='searchbox d-block'>
+                <img src="<?= img_url($info['s_slug'].$info['s_imgindex']); ?>" width="50%" alt="">
+        <a href ="<?= url('site/'.$info['s_id'].'/'.$info['s_slug']); ?>"><h6><?= $info['s_nom']; ?></h6></a>
+                </div>        
             <?php endforeach; ?>
         <?php endif; ?>
-
+       
     <?php endif; ?>
-
+    
 </div>
 
 <?php $content = ob_get_clean() ?> <?php view('template', compact('content')); ?>
